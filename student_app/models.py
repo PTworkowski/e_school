27678ac3d71145eee71ed09f77c7db0ef4c44eff
Joanna Szymanowicz <JoannaSzymanowicz7@gmail.com>
2020-11-16 14:22:37 +0100

    models.py update

--- student_app/models.py
+++ student_app/models.py
@@ -1,4 +1,39 @@
 from django.db import models
-from users_app import models
+from django.db.models import CharField, Model, ForeignKey, CASCADE, BooleanField
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=225)
+    subject = models.ForeignKey(Subjects, on_delete = models.CASCADE)
+    zoom_link = models.CharField(max_length=225)
+    level = models.CharField(max_length=225)
+    teacher = models.ForeignKey(Users, on_delete = models.CASCADE)
+    student = models.ForeignKey(Users, on_delete = models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return
+        pass
+
+class Student_courses(models,Model):
+    student_id = models.ForeignKey(Users, on_delete = models.CASCADE)
+    course_id = models.ForeignKey(Users, on_delete = models.CASCADE)
+    payment  = models.BooleanField()
+
+    def __str__(self):
+        return
+        pass
+
+class Zoom_link(models.Model):
+    meeting_link = models.URLField(max_length=200)
+
+    def __str__(self):
+        return
+        pass
+
+class Course_materials(models.Model):
+    file_id = models.ForeignKey(Files,on_delete = models.CASCADE)
+
+    def __str__(self):
+        return
+        pass
