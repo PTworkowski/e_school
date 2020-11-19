45f7335c0596e6c1ab35370020f95ea358b0ecb3
guzweronika <guzweronika@gmail.com>
2020-11-19 17:13:08 +0100

    corrected models - without Files model

--- administration_app/admin.py
+++ administration_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Subjects
+from .models import Subjects, Course, Schedule
 
 # Register your models here.
 admin.site.register(Subjects)
+admin.site.register(Course)
+admin.site.register(Schedule)
--- administration_app/models.py
+++ administration_app/models.py
@@ -12,7 +12,7 @@ class Subjects(models.Model):
     def __str__(self):
         return self.title
 
-
+#To jest bez sensu... Moze lepiej dzien tygodnia i godzina? Zrobićfunkcje "choices"
 class Schedule(models.Model):
     day = models.DateField(auto_now=False, auto_now_add=False)
     time = models.TimeField(auto_now=False, auto_now_add=False)
@@ -21,20 +21,23 @@ class Schedule(models.Model):
         return f'{self.day} {self.time}'
 
 
+# czy pole schedule nie dać jako many to many?
 class Course(models.Model):
     name = models.CharField(max_length=225)
     subject = models.ForeignKey(Subjects, on_delete=models.CASCADE)
-    zoom_link = models.URLField(max_length=255)
+    zoom_link = models.URLField(max_length=255, blank=True)
     level = models.CharField(max_length=225)
-    teacher = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    student = models.ManyToManyField('users_app.models.MyUser') #Tutaj sie coś nie zgadza
+    teacher = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='teacher')
+    student = models.ManyToManyField('users_app.MyUser', related_name='students')
     schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
+"""
 #Nie wiem czy to jest poprawne, myśle że to jest niepotrzebne????
 class GroupMaterials(models.Model):
     id_filles = models.ManyToManyField('library_app.models.Files')
     id_course = models.ManyToManyField('library_app.models.Files')
+"""
