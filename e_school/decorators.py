4e33ab5e2c2355fd8d74860f0268f7878992310d
PChlasta <paulina.chlasta@gmail.com>
2020-11-21 00:02:04 +0100

    decorators.py - fix

--- e_school/decorators.py
+++ e_school/decorators.py
@@ -35,9 +35,10 @@ def admin_required(func):
 
 def admin_or_teacher_required(func):
     def wrap(request, *args, **kwargs):
-        if not request.user.is_admin and not request.user.is_teacher:
-            return HttpResponseForbidden()
-        return func(request, *args, *kwargs)
+        if request.user.is_admin and request.user.is_teacher:
+            return func(request, *args, *kwargs)
+        return HttpResponseForbidden()
+
 
     wrap.__doc__ = func.__doc__
     wrap.__name__ = func.__name__
